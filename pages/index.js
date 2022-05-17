4b670d8e658565f062085e819c7fcb6c407c9fb1
Peter Garside-Quin <petegq@gmail.com>
2022-05-17 20:52:27 +0100

    Add loader

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Loader from '../components/Loader'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      
+      <Loader show />
     </div>
   )
 }
