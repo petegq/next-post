6743875b452e09992dfd9347577e44e00606e569
Peter Garside-Quin <petegq@gmail.com>
2022-05-18 05:38:05 +0100

    Add extra styling to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,42 @@
 import Link from "next/link";
 
 export default function Navbar() {
-  const { user, username } = {};
+  const user = null;
+  const username = null;
 
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link href="/">
-            <button>FEED</button>
+            <button className="btn-logo">FEED</button>
           </Link>
-
-          {username && (
-            <>
-              <li>
-                <Link href="/admin">
-                  <button>Write Posts</button>
-                </Link>
-              </li>
-              <li>
-                <Link href={`/${username}`}>
-                  <img src={user?.photoURL} />
-                </Link>
-              </li>
-            </>
-          )}
-
-          {!username && (
-            <>
-              <li>
-                <Link href="/enter">
-                  <button>Log in</button>
-                </Link>
-              </li>
-            </>
-          )}
         </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
