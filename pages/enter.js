5ecd1c14445c2770796d6ed4717152aeada04b18
Peter Garside-Quin <petegq@gmail.com>
2022-05-19 16:16:57 +0100

    Add google signin

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,37 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+  <button className="btn-google" onClick={signInWithGoogle}>
+    <img src={"/google.png"} /> Sign in with Google
+  </button>;
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
