672cc582cb58a12d7e94da56e3f53bbd4a4abcad
Peter Garside-Quin <petegq@gmail.com>
2022-05-19 16:53:04 +0100

    Get user data from fb

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { auth, firestore } from "../lib/firebase";
 import { UserContext } from "../lib/context";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe;
+
+    if (user) {
+      const ref = firestore.collection("users").doc(user.uid);
+      unsubscribe = ref.onSnapshot((doc) => {
+        setUsername(doc.data()?.username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    return unsubscribe;
+  }, [user]);
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "jeff" }}>
+    <UserContext.Provider value={{ user, username }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
