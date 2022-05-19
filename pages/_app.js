701beedb3150c7de36a441b33ca09fcdda46e597
Peter Garside-Quin <petegq@gmail.com>
2022-05-19 16:57:58 +0100

    Extract useUserData hook, tidy _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
-import { auth, firestore } from "../lib/firebase";
 import { UserContext } from "../lib/context";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useUserData } from "../lib/hook";
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection("users").doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username);
-      });
-    } else {
-      setUsername(null);
-    }
-
-    return unsubscribe;
-  }, [user]);
+  const userData = useUserData();
 
   return (
-    <UserContext.Provider value={{ user, username }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
